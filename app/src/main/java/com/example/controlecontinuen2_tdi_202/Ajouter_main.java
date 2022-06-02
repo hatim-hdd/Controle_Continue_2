8a1e5eab25d035c370355363e501cef991ac47eb
HATIM HADDAD KART <HADDADKART.HATIM@ofppt-edu.ma>
2022-06-02 16:29:32 +0100

    question 10/c

--- app/src/main/java/com/example/controlecontinuen2_tdi_202/Ajouter_main.java
+++ app/src/main/java/com/example/controlecontinuen2_tdi_202/Ajouter_main.java
@@ -3,6 +3,7 @@ package com.example.controlecontinuen2_tdi_202;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Ajouter_main extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Ajouter_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_main);
+    }
+    public void retourne(View view){
+        this.finish();
     }
 }
