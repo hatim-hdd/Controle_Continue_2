e509dad79e8bb502bf495edec214797d3ddb3460
HATIM HADDAD KART <HADDADKART.HATIM@ofppt-edu.ma>
2022-06-02 15:23:12 +0100

    question 7

--- app/src/main/java/com/example/controlecontinuen2_tdi_202/MainActivity.java
+++ app/src/main/java/com/example/controlecontinuen2_tdi_202/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.controlecontinuen2_tdi_202;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void acces(View view) {
+        Intent i = null;
+        switch (view.getId()){
+            case R.id.btn_Ajout: i=new Intent(MainActivity.this, Ajouter_main.class); break;
+            case R.id.btn_editer: i=new Intent(MainActivity.this, editer_main.class); break;
+            case R.id.btn_lister: i=new Intent(MainActivity.this, aficher_main.class); break;
+        }
+        startActivity(i);
+
     }
 }
