e3e33c9fbe4714709af136f7dc6e0c646c186505
HATIM HADDAD KART <HADDADKART.HATIM@ofppt-edu.ma>
2022-06-02 14:17:04 +0100

    create Database & declare the variables

---
+++ app/src/main/java/com/example/controlecontinuen2_tdi_202/MyDatabase.java
@@ -0,0 +1,37 @@
+package com.example.controlecontinuen2_tdi_202;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabase extends SQLiteOpenHelper {
+
+    public static String DB_NAME="entreprises.db";
+    public static String TABLE_NAME="entreprise";
+    public static String COL1="id";
+    public static String COL2="raison_sociale";
+    public static String COL3="adresse";
+    public static String COL4="capitale";
+
+
+    public MyDatabase(Context c){
+        super(c,DB_NAME,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "create table " + TABLE_NAME + "("+COL1+" integer primary key autoincrement,"+COL2+" TEXT,"+COL3+" TEXT,"+COL4+" double)";
+        sqLiteDatabase.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sql= "DROP TABLE " + TABLE_NAME;
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
+
+    }
+}
