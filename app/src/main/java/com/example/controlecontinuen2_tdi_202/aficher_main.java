8396cf7219d687aeba2372c2e93b0d3b92da4042
HATIM HADDAD KART <HADDADKART.HATIM@ofppt-edu.ma>
2022-06-02 17:00:34 +0100

    question 12

--- app/src/main/java/com/example/controlecontinuen2_tdi_202/aficher_main.java
+++ app/src/main/java/com/example/controlecontinuen2_tdi_202/aficher_main.java
@@ -3,12 +3,30 @@ package com.example.controlecontinuen2_tdi_202;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class aficher_main extends AppCompatActivity {
+    ListView lst;
+    MyDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aficher_main);
+        db = new MyDatabase(this);
+        lst = findViewById(R.id.lst);
+
+        ArrayList<entreprise> entps = MyDatabase.getAllEntreprise(db.getReadableDatabase());
+
+        ArrayList<String> nomentreprise = new ArrayList<>();
+        for(entreprise ee: entps)
+            nomentreprise.add(ee.getId() + " - " + ee.getRaison_sociale());
+
+        ArrayAdapter ad = new ArrayAdapter(this,android.R.layout.simple_list_item_1,nomentreprise);
+
+        lst.setAdapter(ad);
     }
 }
